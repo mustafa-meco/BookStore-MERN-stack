8c477cf14da4bd39ed81931c2d3697fa84592c90
Mustafa Ghoneim <musgamal05@gmail.com>
2023-09-04 15:39:14 +0300

    created first http request

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (request, response) => {
+  console.log(request)
+  return response.status(234).send('Welcome to MERN Stack Tutorial')
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
 });
