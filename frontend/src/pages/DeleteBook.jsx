f400ed4738913846b562d2bfc1641ecb4dcae626
Mustafa Ghoneim <musgamal05@gmail.com>
2023-09-08 16:50:52 +0300

    Delete Book working (fixed bug)

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -4,41 +4,42 @@ import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 
-
 const DeleteBook = () => {
-  const { loading, setloading } = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
   const handleDeleteBook = () => {
-    setloading(true);
+    setLoading(true);
     axios
       .delete(`http://localhost:5555/books/${id}`)
       .then(() => {
-        setloading(false);
+        setLoading(false);
         navigate("/");
       })
       .catch((error) => {
-        setloading(false);
+        setLoading(false);
         alert("An error occurred, please check console");
         console.log(error);
       });
-  return (
-  <div className="p-4">
-    <BackButton />
-    <h1 className="text-3xl my-4">Delete Book</h1>
-    {loading ? <Spinner /> : ""}
-    <div className="flex flex-col item-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
-      <h3 className="text-2xl">Are You Sure You want to delete this book</h3>
+  };
 
-      <button 
-      className="p-4 bg-red-600 text-white m-8 w-full"
-      onClick={handleDeleteBook}
-      >
-        Yes, Delete it
-      </button>
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">Are You Sure You want to delete this book</h3>
+
+        <button
+          className="p-4 bg-red-600 text-white m-8 w-full"
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete it
+        </button>
+      </div>
     </div>
-  </div>
-  )
+  );
 };
 
 export default DeleteBook;
