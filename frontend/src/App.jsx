376a73b8765ff1d16f68f07c114b7ec704f338ed
Mustafa Ghoneim <musgamal05@gmail.com>
2023-09-08 23:49:08 +0300

    TODOs for App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,4 +18,38 @@ const App = () => {
   );
 };
 
+// TODOs for App
+// 1. Add PropTypes
+// 2. Add default props
+// 3. Add tests
+// 4. Add storybook
+// 5. Add styles
+// Ideas for App
+// 1. Add a navbar
+// 2. Add a footer
+// 3. Add a sidebar
+// 4. Add a modal
+// 5. Add a toast
+// 6. Add a spinner
+// 7. Add a loader
+// 8. Add a progress bar
+// 9. Add a slider
+// 10. Add a carousel
+// 11. Add a dropdown
+// creative ideas for App
+// 1. Add a dark mode
+// 2. Add a light mode
+// 3. Add a toggle button for dark and light mode
+// 4. Add a toggle button for navbar
+// 5. Add a toggle button for footer
+// 6. Add a toggle button for sidebar
+// 7. Add a toggle button for modal
+// 8. Add a toggle button for toast
+// 9. Add a toggle button for spinner
+// 10. Add a toggle button for loader
+// 11. Add a toggle button for progress bar
+// 12. Add a toggle button for slider
+// 13. Add a toggle button for carousel
+// 14. Add a toggle button for dropdown
+
 export default App;
